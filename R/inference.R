037e00788d305d64572431dad787f4e2d056a1d3
gujyjean <gujiaying@Jiayings-MacBook-Pro.local>
2016-06-09 12:04:01 -0700

    update inference function and test file
    delete redundant type check and data type converting. inference
    function fit_multinom_dag can only take edgeList object now.

--- R/inference.R
+++ R/inference.R
@@ -30,7 +30,8 @@ fit_multinom_dag <- function(parents, # rename to something else
   data <- as.data.frame(dat)
 
   node <- ncol(data)
-  ### add check that the number of node and the what has been input in parents are consistent!
+  # check that the number of node and the what has been input in parents are consistent
+  if (length(parents) != ncol(data)) {stop(sprintf("Incompatible graph and data! Data has %d columns but graph has %d nodes.", ncol(dat), length(parents)))}
 
   # factorize each observation
   for (i in 1:node){
@@ -38,39 +39,10 @@ fit_multinom_dag <- function(parents, # rename to something else
     data[,i] <- factor(data[,i],levels=level)
   }
 
-  # get edge_list
-  ### if parents is an edgeList object
-  if (sparsebnUtils::is.edgeList(parents)) {
-    if (length(parents) != ncol(data)) {stop(sprintf("Incompatible graph and data! Data has %d columns but graph has %d nodes.", ncol(dat), length(parents)))}
-    # adjMatrix <- sparsebnUtils::get.adjacency.matrix(parents)
-    edge_list <- parents
-  }
-  if (sparsebnUtils::is.sparsebnFit(parents)) {
-    if (length(parents$edges) != ncol(data)) {stop(sprintf("Incompatible graph and data! Data has %d columns but graph has %d nodes.", ncol(dat), length(parents$edges)))}
-    # adjMatrix <- sparsebnUtils::get.adjacency.matrix(parents)
-    edge_list <- parents$edges
-  }
-  ### if parents is an adjacency matrix
-  if (is.matrix(parents) || class(parents) == "dgCMatrix") {
-    if (sum(((parents!=0)+(parents!=1))!=1)) stop("input matrix must be an adjacency matrix, where 1 means there exists an edge and 0 means there is no edge!")
-    if (ncol(parents) != ncol(data)) {stop(sprintf("Incompatible graph and data! Data has %d columns but graph has %d nodes.", ncol(dat), ncol(parents)))}
-    parents <- as.matrix(parents)
-    # adjMatrix <- parents
-    graph_list <- lapply(seq_len(ncol(parents)), function(i) parents[,i])
-    edge_list <- lapply(graph_list, function(x){
-      return(which(x==1))
-    })
-    edge_list <- sparsebnUtils::edgeList(edge_list)
-  }
-  ### throw an error if parents is neither an adjacency matrix nor an edgeList object
-  if (!(sparsebnUtils::is.edgeList(parents) || sparsebnUtils::is.sparsebnFit(parents) || is.matrix(parents) || class(parents) == "dgCMatrix")) stop("parents must be an edgeList object or sparsebnFit object or an adjacency matrix!")
-
   # subtract dependent and independent variables for each regression
-  # coef <- vector("list", length = node)
   coef <- lapply(seq_len(node), function(i){integer(0)})
   for (i in 1:node){
-    # x_ind <- which(adjMatrix[, i]==1)
-    x_ind <- edge_list[[i]] # index for independant variable
+    x_ind <- parents[[i]] # if inputs are only edgeList object
     if (length(x_ind)!=0) { # do nothing if a node has no parents
       fit <- nnet::multinom(data[, c(i, x_ind)], trace = FALSE) # Why does this work / should we do this?
       coef_vec <- coef(fit)
--- tests/testthat/test-fit_multinom_dag.R
+++ tests/testthat/test-fit_multinom_dag.R
@@ -41,12 +41,12 @@ databn <- sparsebnUtils::sparsebnData(data, ivn = ivn, type = "discrete")
 final <- cd.run(databn)
 final.dag <- final[[length(final)]]
 edge_list<- final.dag$edges
-adj_matrix <- sparsebnUtils::get.adjacency.matrix(final.dag)
-matrix <- matrix(adj_matrix, nrow = 6)
+# adj_matrix <- sparsebnUtils::get.adjacency.matrix(final.dag)
+# matrix <- matrix(adj_matrix, nrow = 6)
 
 # test
 test_that("fit_multinom_dag can take empty graphs", {
-  empty_graph <- final[[1]]
+  empty_graph <- final[[1]]$edges
   expect_error(fit_multinom_dag(empty_graph, n_levels, data), NA)
 
   ### test if I input a graph with a single node, will the algorithm work.
@@ -55,47 +55,26 @@ test_that("fit_multinom_dag can take empty graphs", {
   generate_empty_edgeList <- function(){
     sparsebnUtils::edgeList(list(integer(0)))
   }
-  generate_empty_sparsebnFit <- function(){
-    li <- list(edges = generate_empty_edgeList(), lambda = 1, nedge = 0, pp = 1, nn = 10, time = 1)
-    sparsebnUtils::sparsebnFit(li)
-  }
-  single_node <- generate_empty_sparsebnFit()
+  single_node <- generate_empty_edgeList()
   expect_error(fit_multinom_dag(single_node, n_levels=4, data_single), NA)
 })
 
-test_that("fit_multinom_dag can run with different types of input", {
-  ### fit_multinom_dag can accept a sparsebnFit object as an input
-  expect_error(fit_multinom_dag(final.dag, n_levels, data), NA)
+test_that("fit_multinom_dag can run", {
   ### fit_multinom_dag can accept an edgeList object as an input
   expect_error(fit_multinom_dag(edge_list, n_levels, data), NA)
-  ### fit_multinom_dag can accept an adjacency matrix as an input
-  expect_error(fit_multinom_dag(matrix, n_levels, data), NA)
-  ### fit_multinom_dag can accept an adjacency matrix of class dgCMatrix as an input
-  expect_error(fit_multinom_dag(adj_matrix, n_levels, data), NA)
 
-  adj_list <- apply(matrix, 1, function(x){list(x)})
   ### throw an error if fit_multinom_dag has the wrong input
-  expect_error(fit_multinom_dag(adj_list, n_levels, data))
-
-  ### four outputs should all be same
-  out_sparsebnFit <- fit_multinom_dag(final.dag, n_levels, data)
-  out_edgeList <- fit_multinom_dag(edge_list, n_levels, data)
-  out_matrix <- fit_multinom_dag(matrix, n_levels, data)
-  out_adjMatrix <- fit_multinom_dag(adj_matrix, n_levels, data)
-
-  expect_equal(out_sparsebnFit, out_edgeList)
-  expect_equal(out_edgeList, out_matrix)
-  expect_equal(out_matrix, out_adjMatrix)
+  expect_error(fit_multinom_dag(final.dag, n_levels, data))
 })
 
 test_that("fit_multinom_dag output the right result", {
-  out <- fit_multinom_dag(final.dag, n_levels, data)
+  out <- fit_multinom_dag(edge_list, n_levels, data)
 
   ### length of output should be the number of variables
   expect_equal(length(out), node)
   ### legnth of each element should be the numeber of parents
   for (i in 1:node) {
-    expect_equal(length(out[[i]]), sum(matrix[, i])+(sum(matrix[, i])!=0))
+    expect_equal(length(out[[i]]), length(edge_list[[i]])+(length(edge_list[[i]])!=0))
   }
   ### randomly check some entries
   expect_equal(dim(out[[2]][[1]]$coef), c(1, 1))
